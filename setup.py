082e44898dd20c41b7ce88fc6f245cc79464e0d5
Jimmy John <jjohn@hirevue.com>
2014-01-28 23:49:18 -0800

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Tapi",
-    version = "0.1.6",
+    version = "0.1.7",
     packages = find_packages(),
     scripts = ['tapi.py'],
 
